1b37a7fe69b3006781a54ae828058670a45a95fe
Angel Sierra Vega <angel.sierra.vega@grupoindie.com>
2018-01-18 21:09:18 -0600

    Reconstructed project structure.

---
+++ src/GIndie/ScriptGenerator/Bootstrap3/Plugins/UnitTest/HandlerProject.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * SG-Bootstrap3 - HandlerProject
+ *
+ * @copyright (c) 2018 Angel Sierra Vega. Grupo INDIE.
+ *
+ * @package ScriptGenerator
+ * @subpackage Bootstrap3
+ *
+ * @version SG-BTSP3.00.00 18-01-18 Empty [class/trait/interface/file] created.
+ */
+
+namespace GIndie\ScriptGenerator\Bootstrap3\Plugins\UnitTest;
+
+/**
+ * Description of HandlerProject
+ *
+ * @author Angel Sierra Vega <angel.sierra@grupoindie.com>
+ */
+class HandlerProject
+{
+    //put your code here
+}
---
+++ src/GIndie/ScriptGenerator/Bootstrap3/autoloader.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * SG-Bootstrap3 - autoloader
+ *
+ * @copyright (c) 2018 Angel Sierra Vega. Grupo INDIE.
+ *
+ * @package ScriptGenerator
+ * @subpackage Bootstrap3
+ *
+ * @version SG-BTSP3.00.00 18-01-18 Empty [class/trait/interface/file] created.
+ */
+
+namespace GIndie\ScriptGenerator\Bootstrap3;
