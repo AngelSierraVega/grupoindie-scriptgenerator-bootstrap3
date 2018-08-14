a23f641e598e21e891b826afd0ab62511120010b
Angel Sierra Vega <angel.sierra.vega@grupoindie.com>
2018-08-14 12:38:50 -0500

    18-06-23: 18-06-24: - Functional Grid 18-08-14 - DOING Tooltip helper class (0A.F3)
    -------------------------------PACKAGE---------------------------------|
     \GIndie\ScriptGenerator\Bootstrap3 v 0 (2.2690945947115E+32)
    -------------------------------VERSIONS--------------------------------|
    [0A.00] AlphaCero....| Functional project
    [0B.00] BetaCero.....| Main funcionality
    [0C.00] One..........| Final version

---
+++ Javascript/Tooltip.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * GI-SG3-Bootstrap3-DVLP - Tooltip
+ *
+ * @author Angel Sierra Vega <angel.sierra@grupoindie.com>
+ * @copyright (c) 2018 Angel Sierra Vega. Grupo INDIE.
+ *
+ * @package \GIndie\ScriptGenerator\Bootstrap3
+ *
+ * @since 18-08-14
+ * @version DOING 0A.F3
+ */
+
+namespace GIndie\ScriptGenerator\Bootstrap3\Javascript;
+
+/**
+ * Description of Tooltip
+ *
+ * @author Angel Sierra Vega <angel.sierra@grupoindie.com>
+ */
+class Tooltip
+{
+    //put your code here
+}
