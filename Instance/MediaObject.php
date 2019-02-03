a3cbf8df60d1d0744d5f69392411b04090c6b196
Angel Sierra Vega <angel.sierra.vega@grupoindie.com>
2019-02-02 21:24:04 -0600

    CURRENT 19-02-03: - DOING Instance\MediaObject

---
+++ Instance/MediaObject.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * GI-SG3-Bootstrap3-DVLP - MediaObject
+ *
+ * @author Angel Sierra Vega <angel.sierra@grupoindie.com>
+ * @copyright (c) 2019 Angel Sierra Vega. Grupo INDIE.
+ *
+ * @package GIndie\ScriptGenerator\Bootstrap3
+ *
+ * @since 19-02-03
+ * @version 00.00
+ */
+
+namespace GIndie\ScriptGenerator\Bootstrap3\Instance;
+
+/**
+ * Abstract object styles for building various types of components (like blog comments, Tweets, 
+ * etc) that feature a left- or right-aligned image alongside textual content.
+ *
+ * @link <https://getbootstrap.com/docs/3.3/components/#media>
+ */
+class MediaObject
+{
+    //put your code here
+}
